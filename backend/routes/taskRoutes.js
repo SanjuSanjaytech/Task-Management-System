a84815a81b4c3498037c95ae0ff3ea63607fce29
Sanjay <sanjutech9959@gmail.com>
2026-01-20 16:27:45 +0530

    Implemented the routes for GenAi

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,5 +1,11 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
+
+
 const auth = require('../middlewares/authMiddleware');
+const Task = require("../models/Task")
+const { extractTaskfromText } = require('../utils/gemini');
+
 const {
     createTask, getTasks, updateTask, deleteTask, assignTask, dashboard
 } = require('../controllers/taskController');
@@ -10,5 +16,38 @@ router.get('/dashboard', auth, dashboard);
 router.put('/:id', auth, updateTask);
 router.delete('/:id', auth, deleteTask);
 router.post('/:id/assign', auth, assignTask);
+
+
+
+//  AI Route
+
+router.post('/ai-create', auth, async (req, res) => {
+    try{
+        const { text } = req.body;
+
+        
+        if (!text) {
+            return res.status(400).json({ message: "Text is required" });
+        }
+
+        const aiTask = await extractTaskfromText(text);
+
+        const task = await Task.create( {
+            title: aiTask.title,
+            description: aiTask.description,
+            dueDate: aiTask.dueDate,
+            priority: aiTask.priority,
+            user: req.user._id
+        })
+
+        res.status(201).json(task)
+
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({messages: 'AI Task creation failed'});
+    }
+
+})
 
 module.exports = router;
